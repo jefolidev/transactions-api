5960695b935dbabfcb28833d8bd88ae0934cbd86
jefolidev <jefoliveira279@gmail.com>
2025-01-20 08:02:25 -0300

    adicionado monitoramente de criação de transações por cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -12,10 +12,22 @@ export async function transactionRoutes(app: FastifyInstance) {
 
     const { title, amount, type } = newTransactionBodySchema.parse(req.body)
 
+    let sessionId = req.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = crypto.randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // ! 7 Dias
+      })
+    }
+
     await knexDb('transactions').insert({
       id: crypto.randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
