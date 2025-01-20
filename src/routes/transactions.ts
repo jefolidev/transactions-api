fb0faaffc8432eba8b97ca780f82b2750bdf9c4f
jefolidev <jefoliveira279@gmail.com>
2025-01-20 01:07:51 -0300

    adicionado rota para obter as tranasações, cálculo das transações e obter transação por id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -16,9 +16,37 @@ export async function transactionRoutes(app: FastifyInstance) {
       id: crypto.randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
-      created_at: new Date(),
     })
 
     return reply.status(201).send()
+  })
+
+  app.get('/', async () => {
+    const transactions = await knexDb('transactions').select()
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (req) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(req.params)
+
+    // ? O ".first()" diz que só possui um resultado, então retorna somente o id
+    const transaction = await knexDb('transactions').where('id', id).first()
+
+    return transaction
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knexDb('transactions')
+      .sum('amount', { as: 'totalAmount' })
+      .first()
+
+    return summary
   })
 }
